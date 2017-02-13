52399be201d5968f0bd3c68e97c9bd15ff8fe031
Tyler Reid <tdreid@email.wm.edu>
2017-02-13 21:26:15 +0000

    Upload new file

---
+++ code/index.php
@@ -0,0 +1,12 @@
+
+<html>
+<body>
+<h1>small page to insert some mysql data from php</h1>
+<form action="insert.php" method="post">
+firstname: <input type="text" name="fname" /><br><br>
+lastname: <input type="text" name="lname" /><br><br>
+
+<input type="submit" />
+</form>
+</body>
+</html>
