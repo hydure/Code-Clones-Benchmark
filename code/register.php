2f68dbc98daa7f1d0b8986bb44cf89179674acf4
Jason Kimko <jskimko@email.wm.edu>
2017-02-27 19:14:07 +0000

    Upload register.php

---
+++ code/register.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+
+<?php
+$con = mysqli_connect('localhost', 'root', '*XMmysq$', 'cc_bench');
+if(!$con) {
+        die('coult not connect: ' . mysqli_connect_error());
+}
+
+$sql="INSERT INTO Account (username, password)
+        VALUES('$_POST[username]', '$_POST[password]')";
+
+if (!mysqli_query($con, $sql)) {
+        die("Error: " . mysqli_error($con));
+}
+echo "1 account added.";
+
+mysqli_close($con)
+?>
+</body>
+</html>
