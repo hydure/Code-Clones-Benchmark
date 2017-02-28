39655acae76a5fc8fe46af4b755850c4efd65b67
Zachary Allison <ztallison@email.wm.edu>
2017-02-28 02:14:42 +0000

    Replace verify.php

--- code/Webpages_and_Webpage_Accessories/verify.php
+++ code/Webpages_and_Webpage_Accessories/verify.php
@@ -1,7 +1,8 @@
 <?php
 require_once 'class.user.php';
 $user = new USER();
-
+if(isset($_POST['btn-signup']))
+{
 if(empty($_GET['id']) && empty($_GET['code']))
 {
  $user->redirect('index.php');
@@ -14,35 +15,47 @@ if(isset($_GET['id']) && isset($_GET['code']))
  
  $statusY = "Y";
  $statusN = "N";
- 
- $stmt = $user->runQuery("SELECT userID,userStatus FROM RegistrationInfo WHERE userID=:uID AND tokenCode=:code LIMIT 1");
+ $vercode = trim($_POST['txtvercode']);
+
+ $stmt = $user->runQuery("SELECT userID,userStatus FROM Accounts WHERE userID=:uID AND tokenCode=:code LIMIT 1");
  $stmt->execute(array(":uID"=>$id,":code"=>$code));
  $row=$stmt->fetch(PDO::FETCH_ASSOC);
  if($stmt->rowCount() > 0)
  {
-  if($row['userStatus']==$statusN)
-  {
-   $stmt = $user->runQuery("UPDATE RegistrationInfo SET userStatus=:status WHERE userID=:uID");
-   $stmt->bindparam(":status",$statusY);
-   $stmt->bindparam(":uID",$id);
-   $stmt->execute(); 
+  $confirmcode = $row['vercode'];
+   if($confirmcode == $vercode)
+   {
+    if($row['userStatus']==$statusN)
+    {
+     $stmt = $user->runQuery("UPDATE Accounts SET userStatus=:status WHERE userID=:uID");
+     $stmt->bindparam(":status",$statusY);
+     $stmt->bindparam(":uID",$id);
+     $stmt->execute(); 
    
-   $msg = "
-             <div class='alert alert-success'>
-       <button class='close' data-dismiss='alert'>&times;</button>
-       <strong>WoW !</strong>  Your Account is Now Activated : <a href='index.php'>Login here</a>
-          </div>
-          "; 
+     $msg = "
+               <div class='alert alert-success'>
+         <button class='close' data-dismiss='alert'>&times;</button>
+         <strong>WoW !</strong>  Your Account is Now Activated : <a href='index.php'>Login here</a>
+            </div>
+            "; 
+    }
+    else
+    {
+     $msg = "
+               <div class='alert alert-error'>
+         <button class='close' data-dismiss='alert'>&times;</button>
+         <strong>sorry !</strong>  Your Account is allready Activated : <a href='index.php'>Login here</a>
+            </div>
+            ";
+    }
   }
   else
   {
-   $msg = "
-             <div class='alert alert-error'>
+   $msg = "<div class='alert alert-block'>
        <button class='close' data-dismiss='alert'>&times;</button>
-       <strong>sorry !</strong>  Your Account is allready Activated : <a href='index.php'>Login here</a>
-          </div>
-          ";
-  }
+       <strong>Invalid Code!</strong> 
+       </div>";
+  } 
  }
  else
  {
@@ -52,9 +65,9 @@ if(isset($_GET['id']) && isset($_GET['code']))
       <strong>sorry !</strong>  No Account Found : <a href='signup.php'>Signup here</a>
       </div>
       ";
- } 
+ }
 }
-
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -72,6 +85,11 @@ if(isset($_GET['id']) && isset($_GET['code']))
   </head>
   <body id="login">
     <div class="container">
+	<form class="form-verify" method="post">
+	<h2 class="form-verify-heading">Input Verification Code</h2>
+	<input type="text" class="input-block-level" placeholder="Verification code" name="txtvercode" required />
+	<button class="btn btn-large btn-primary" type="submit" name="btn-signup">Verify</button>
+	</form>
   <?php if(isset($msg)) { echo $msg; } ?>
     </div> <!-- /container -->
     <script src="vendors/jquery-1.9.1.min.js"></script>
