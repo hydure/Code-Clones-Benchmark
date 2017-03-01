ebfe069bf4fe547c74ab5d5827c903a39790f992
Jason Kimko <jskimko@email.wm.edu>
2017-03-01 21:24:19 +0000

    Change script.sh -> run_nicad.sh

--- code/examples/run_nicad.php
+++ code/examples/run_nicad.php
@@ -2,7 +2,7 @@
 <body>
 
 <?php
-$cmd="/home/pi/Code-Clones-Benchmark/code/examples/script.sh $_POST[username] $_POST[password] $_POST[python]";
+$cmd="/home/pi/Code-Clones-Benchmark/code/examples/run_nicad.sh $_POST[username] $_POST[password] $_POST[python]";
 echo shell_exec("$cmd");
 ?>
 
