1621cdc06e99ba1ed51735733a866415ad24e571
Colin Lightfoot <cflightfoot@gmail.com>
2017-03-03 04:25:55 +0000

    Update CCBTools.php

--- code/CCBTools.php
+++ code/CCBTools.php
@@ -14,7 +14,7 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
 ?>
 
-c<!-- Latest compiled and minified CSS -->
+<!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
 <!-- jQuery library -->
