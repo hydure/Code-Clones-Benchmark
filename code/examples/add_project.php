69defc2fb4f191b6434fb42662a98fdb0981b140
Jason Kimko <jskimko@email.wm.edu>
2017-03-03 03:37:40 +0000

    Update add_project.php and gets head commit number.

--- code/examples/add_project.php
+++ code/examples/add_project.php
@@ -9,17 +9,23 @@ if(!$con) {
 
 $title = exec("echo $_POST[url] | sed 's:.*\.com/[^/]*/::'");
 
+if ("$_POST[commit]" == "head") {
+	$commit = exec("/home/pi/Code-Clones-Benchmark/code/examples/get_head_commit.sh $_POST[url]");
+} else {
+	$commit = $_POST[commit];
+}
+
 date_default_timezone_set('America/New_York');
 $date = date('Y-m-d H:i:s');
 
 if ("$_POST[private]" == "1") {
-	$ownership = 0;
+	$ownership = -1;
 } else {
-	$ownership = 0;
+	$ownership = -1;
 }
 
 $sql="INSERT INTO Projects (title, url, commit, uploaded, ownership)
-        VALUES('$title', '$_POST[url]', '$_POST[commit]', '$date', $ownership)";
+        VALUES('$title', '$_POST[url]', '$commit', '$date', $ownership)";
 
 if (!mysqli_query($con, $sql)) {
         die("Error: " . mysqli_error($con));
