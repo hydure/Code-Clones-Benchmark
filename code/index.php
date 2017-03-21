9e9251034cab41f4d513049064b7d68b572e9783
Colin Lightfoot <cflightfoot@gmail.com>
2017-03-21 02:38:29 +0000

    Update index.php

--- code/index.php
+++ code/index.php
@@ -5,7 +5,7 @@ $user_login = new USER();
 
 if($user_login->is_logged_in()!="")
 {
- $user_login->redirect('CCBHome1.1.php');
+ $user_login->redirect('CCBHome.php');
 }
 
 if(isset($_POST['btn-login']))
@@ -15,7 +15,7 @@ if(isset($_POST['btn-login']))
  echo "$uname $upass entered";
  if($user_login->login($uname,$upass))
  {
-  $user_login->redirect('CCBHome1.1.php');
+  $user_login->redirect('CCBHome.php');
  }
 }
 ?>
