9cf07563558808ce85038d4af6b5bd774f3de023
Jason Kimko <jskimko@email.wm.edu>
2017-03-22 04:37:01 +0000

    Download nicad output to pi.

--- code/examples/run_nicad.php
+++ code/examples/run_nicad.php
@@ -3,7 +3,12 @@
 
 <?php
 $cmd="/home/pi/Code-Clones-Benchmark/code/examples/run_nicad.sh $_POST[python]";
-echo shell_exec("$cmd");
+$nicad_raw = shell_exec("$cmd");
+#echo $nicad_raw;
+
+$datasetID = 1;
+$file = "/home/pi/MyNAS/".$datasetID."_nicad.html";
+file_put_contents($file, $nicad_raw);
 ?>
 
 </body>
