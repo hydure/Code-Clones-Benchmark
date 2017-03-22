8de428ed4956ca6d57d37e6e5c28386527d61fab
Jason Kimko <jskimko@email.wm.edu>
2017-03-22 08:11:09 +0000

    Correctly checks for no file selection.

--- code/upload_project.php
+++ code/upload_project.php
@@ -1,7 +1,7 @@
  <?php
  session_start();
 // Check if a file has been uploaded
-if(isset($_FILES['uploaded_file'])) {
+if(isset($_FILES['uploaded_file']) && $_FILES['uploaded_file']['size'] != 0) {
     // Make sure the file was sent without errors
     if($_FILES['uploaded_file']['error'] == 0) {
         // Connect to the database
@@ -38,52 +38,46 @@ if(isset($_FILES['uploaded_file'])) {
  
         // Execute the query
         $result = $dbLink->query($query);
- 	//get the project id of uploaded file
-	if($result){
-	$query2 = "Select projectID from Projects where userId='{$userId}' AND title='{$name}' AND ownership='{$ownership}'";
+ 	    //get the project id of uploaded file
+	    if($result){
+	        $query2 = "Select projectID from Projects where userId='{$userId}' AND title='{$name}' AND ownership='{$ownership}'";
 
-	$return = $dbLink->query($query2);
-	$row = $return->fetch_assoc();
-	$drname = $row['projectID'];
-	$path = "/home/pi/MyNAS/files/p$drname";
-    //$path = "/home/reid/MyNAS/files/p$drname";
-	$file = $_FILES['uploaded_file']['tmp_name'];
-	if(!mkdir($path, 0700, true)){
-		echo 'An error accured while the file was being uploaded. '
- 		. 'Could not create directory';
-		//Delete from database if failed to create directory
-		$delete = "DELETE FROM Projects where projectID=$drname";
-		$dbLink->query($delete);
-	}
-	else{
-			
-		if(!move_uploaded_file($file, "$path/$name")){
-			$delete = "DELETE FROM Projects where projectID=$drname";
-			$dbLink->query($delete);
-			echo 'An error accured while the file was being uploaded. '
- 			. 'Could not save file';
-		}
+	        $return = $dbLink->query($query2);
+	        $row = $return->fetch_assoc();
+	        $drname = $row['projectID'];
+	        $path = "/home/pi/MyNAS/files/p$drname";
+            //$path = "/home/reid/MyNAS/files/p$drname";
+	        $file = $_FILES['uploaded_file']['tmp_name'];
+	        if(!mkdir($path, 0700, true)){
+		        echo 'An error accured while the file was being uploaded. '
+ 		        . 'Could not create directory';
+		        //Delete from database if failed to create directory
+		        $delete = "DELETE FROM Projects where projectID=$drname";
+		        $dbLink->query($delete);
+	        } else{
+		        if(!move_uploaded_file($file, "$path/$name")){
+			        $delete = "DELETE FROM Projects where projectID=$drname";
+			        $dbLink->query($delete);
+			        echo 'An error accured while the file was being uploaded. '
+ 			            . 'Could not save file';
+		        } else{
+           		    echo 'Success! Your file was successfully added!';
+      		    }
+	        }
+	    } else{
+	        echo "An error occured while the file was being uploaded.";
+	    }
 
-     		else{
-           		 echo 'Success! Your file was successfully added!';
-      		}
-	}
-	}
-	else{
-	echo "An error occured while the file was being uploaded.";
-	}
-
-    }
-    else {
+        // Close the mysql connection
+        $dbLink->close();
+    } else {
         echo 'An error accured while the file was being uploaded. '
            . 'Error code: '. intval($_FILES['uploaded_file']['error']);
     }
  
-    // Close the mysql connection
-    $dbLink->close();
 }
 else {
-    echo 'Error! A file was not sent!';
+    echo 'Please choose a file to upload.';
 }
  
 // Echo a link back to the main page
