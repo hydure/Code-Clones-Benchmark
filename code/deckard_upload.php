daaa74ef45274030839e5afecfe482463e0e4f97
tylerreid <reid@reidOpenSUSE>
2017-04-04 18:45:48 -0400

    parses and uploads deckard file and line to Clones table

---
+++ code/deckard_upload.php
@@ -0,0 +1,37 @@
+<?php
+
+$con = new mysqli('127.0.0.1', 'root', '*XMmysq$', 'cc_bench');
+if(mysqli_connect_errno()) {
+    die("MySQL connection failed: ". mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM Clones ORDER BY cloneID DESC limit 1;";
+$query = mysqli_query($con, $sql);
+$cloneID = mysqli_fetch_assoc($query)['cloneID'] + 1;
+$datasetID = 1;
+$detector = 'Deckard';
+
+$handle = fopen("/home/reid/Code-Clones-Benchmark/artifacts/DeckardTesting/clusters/post_cluster_vdb_30_0_allg_0.95_30", "r");
+if ($handle) {
+	while (($line = fgets($handle)) != false) {
+		if ($line != "\n") {
+			$parsed = explode(" ", $line);
+			$file = $parsed[1];
+			$index = explode(":", substr($parsed[2], 4));
+			$start = $index[1];
+			$end = intval(($index[2])) + intval($start);
+
+			$sql = "INSERT INTO Clones (cloneID, datasetID, file, start, end, detector )
+			VALUES ( '{$cloneID}', '{$datasetID}', '{$file}', '{$start}', '{$end}', '{$detector}')";
+			if (!mysqli_query($con, $sql)) {
+        		die("Error: " . mysqli_error($con));
+			}
+		} else { 
+			$cloneID += 1;
+		}
+	}
+
+	fclose($handle);
+}
+mysqli_close($con);
+?>
