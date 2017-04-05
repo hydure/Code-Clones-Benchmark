af8bb74fedb001c963b716bdc107ec4b4071ad13
Charles Rouse <crouse@email.wm.edu>
2017-04-05 23:31:10 +0000

    Update CCBHome.php

--- code/CCBHome.php
+++ code/CCBHome.php
@@ -68,7 +68,7 @@ $(document).ready(function() {
       <div class="row row-offcanvas row-offcanvas-left">
         
         <!-- sidebar -->
-        <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar" role="navigation">
+        <div class="col-xs-6 col-sm-2 sidebar-offcanvas" id="sidebar" role="navigation">
             <ul class="nav">
               <li class="active"><a href="#">Home</a></li>
               <li><a href="CCBProjects.php">Projects</a></li>
