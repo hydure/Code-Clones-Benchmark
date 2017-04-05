dcb5f2911d818f7512422588d359ebac6787c13b
Charles Rouse <crouse@email.wm.edu>
2017-04-05 23:28:02 +0000

    Update CCBContacts.php

--- code/CCBContacts.php
+++ code/CCBContacts.php
@@ -65,7 +65,7 @@ $(document).ready(function() {
       <div class="row row-offcanvas row-offcanvas-left">
         
         <!-- sidebar -->
-        <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar" role="navigation">
+        <div class="col-xs-6 col-sm-2 sidebar-offcanvas" id="sidebar" role="navigation">
             <ul class="nav">
               <li><a href="CCBHome.php">Home</a></li>
               <li><a href="CCBProjects.php">Projects</a></li>
