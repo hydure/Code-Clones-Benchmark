a50a4c318bde00e65751573332f54abffb0725be
Jason Kimko <jskimko@email.wm.edu>
2017-04-07 05:24:49 +0000

    Add Deckard support thru site. Does not store clones.

--- code/run_detectors.php
+++ code/run_detectors.php
@@ -2,6 +2,7 @@
 <body>
 
 <?php
+session_start();
 if (!empty($_POST['detector'])) {
 
 
@@ -13,16 +14,42 @@ foreach($_POST['detector'] as $detector) {
 echo "> on dataset ".$_POST['datasetSelect']."<br>";
 
 foreach($_POST['detector'] as $detector) {
+    $userID = intval($_SESSION['userSession']);
+
+    # connect to database
+    $con = mysqli_connect('localhost', 'root', '*XMmysq$', 'cc_bench');
+    if(!$con) {
+            die('could not connect: ' . mysqli_connect_error());
+    }
+    
+    # obtain projectIDs from selected dataset
+    $sql="SELECT projectID FROM Datasets WHERE datasetID=".$_POST['datasetSelect'];
+    $pIDs = mysqli_query($con, $sql);
+    if (!$pIDs) {
+        die("Error: " . mysqli_error($con));
+    }
+
+    #update tags in Datasets
+    date_default_timezone_set('America/New_York');
+    $date = date('Y-m-d H:i:s');
+    $sql = "UPDATE Datasets SET Nicad_flag=1, submit_date='$date' WHERE datasetID=".$_POST['datasetSelect'];
+    if(!$con->query($sql))
+        echo "failed to update dataset info";
+
+        /***************************
+        *                          *
+        *          NICAD           *
+        *                          *
+        ***************************/
     if ($detector == "nicad") {
+        if (empty($_POST['n_language'])) {
+            echo "Please select a language.<br>";
+            break;
+        }
+
         # get language
         foreach ($_POST['n_language'] as $lang) {
             if ($lang != "") break;
-        }
-        
-        # connect to database
-        $con = mysqli_connect('localhost', 'root', '*XMmysq$', 'cc_bench');
-        if(!$con) {
-                die('could not connect: ' . mysqli_connect_error());
         }
         
         # check if dataset has been examined already
@@ -31,19 +58,9 @@ foreach($_POST['detector'] as $detector) {
             " AND language='$lang'");
         if ($history->num_rows > 0) {
             echo "You ran this dataset already!<br>";
-            exit;
-        }# else {
-         #   echo "blah<br>";
-         #   exit;
-        #}
-
-        # obtain projectIDs from selected dataset
-        $sql="SELECT projectID FROM Datasets WHERE datasetID=".$_POST['datasetSelect'];
-        $pIDs = mysqli_query($con, $sql);
-        if (!$pIDs) {
-            die("Error: " . mysqli_error($con));
+            break;
         }
-
+       
         # build arguments: language projectID1 url1 ...
         $args="$lang ".$_POST['datasetSelect'];
         while($row = mysqli_fetch_array($pIDs)) {
@@ -56,13 +73,6 @@ foreach($_POST['detector'] as $detector) {
             $args="$args ".mysqli_fetch_array($pURL)['url'];
         }
 
-        #update tags in Datasets
-        date_default_timezone_set('America/New_York');
-        $date = date('Y-m-d H:i:s');
-        $sql = "UPDATE Datasets SET Nicad_flag=1, submit_date='$date' WHERE datasetID=".$_POST['datasetSelect'];
-        if(!$con->query($sql))
-            echo "failed to update dataset info";
-
         # run nicad
         $nicad_path="/home/clone/nicad.sh";
         #$file = "/home/pi/MyNAS/nicad/".$_POST['datasetSelect'].".html";
@@ -74,7 +84,6 @@ foreach($_POST['detector'] as $detector) {
         # write nicad output to file
         $file = "/home/pi/MyNAS/nicad/".$_POST['datasetSelect'].".html";
         file_put_contents($file, $nicad_raw);
-        shell_exec("chmod 600 $file");
 
         # get clones
         $clones=`./parse.sh $file`;
@@ -98,9 +107,9 @@ foreach($_POST['detector'] as $detector) {
                 $end=$clones[$i++];
 
                 $sql="INSERT INTO Clones (cloneID, datasetID, projectID, ".
-                    "file, start, end, sim, detector, language) ".
-                    "VALUES($cloneID, $datasetID, $projectID, '$file', $st, 
-                    $end, $sim, 'nicad', '$lang')";
+                    "userID, file, start, end, sim, detector, language) ".
+                    "VALUES($cloneID, $datasetID, $projectID, '$userID', '$file', 
+                    $st, $end, $sim, 'nicad', '$lang')";
 
                 if (!mysqli_query($con, $sql)) {
                     die("Error: " . mysqli_error($con));
@@ -113,9 +122,98 @@ foreach($_POST['detector'] as $detector) {
         echo "There were $num_classes classes of clones.<br>";
         echo "Added $num_clones clones pairs.";
 
-        mysqli_close($con);
+        /***************************
+        *                          *
+        *          DECKARD         *
+        *                          *
+        ***************************/
     } else if ($detector == 'deckard') {
+        if (empty($_POST['d_language'])) {
+            echo "Please select a language.<br>";
+            break;
+        }
+
+        # get language
+        foreach ($_POST['d_language'] as $lang) {
+            if ($lang != "") break;
+        }
+        
+    # check if dataset has been examined already
+    $history = mysqli_query($con, "SELECT cloneID FROM Clones WHERE ".
+        "datasetID=".$_POST['datasetSelect']." AND detector='nicad'".
+        " AND language='$lang'");
+    if ($history->num_rows > 0) {
+        echo "You ran this dataset already!<br>";
+        exit;
     }
+
+        # build arguments: language projectID1 url1 ...
+        $args="$lang ".$_POST['datasetSelect'];
+        while($row = mysqli_fetch_array($pIDs)) {
+            $pURL = mysqli_query($con, "SELECT url FROM Projects WHERE projectID=".$row['projectID']);
+            if (!$pURL) {
+                die("Error: " . mysqli_error($con));
+            }
+
+            $args="$args ".$row['projectID'];
+            $args="$args ".mysqli_fetch_array($pURL)['url'];
+        }
+
+        # run deckard
+        $deckard_path="/home/clone/deckard.sh";
+        #$file = "/home/pi/MyNAS/nicad/".$_POST['datasetSelect'].".html";
+        #$cmd="ssh -o StrictHostKeyChecking=no clone@45.33.96.10 '$nicad_path $args' >$file 2>/dev/null &";
+        $cmd="ssh -o StrictHostKeyChecking=no clone@45.33.96.10 '$deckard_path $args' | grep -v 'known hosts'";
+        $deckard_raw = shell_exec($cmd);
+        echo "$deckard_raw<br>";
+
+        if (preg_match('/^Error: there are no/', $deckard_raw)) {
+            break;
+        }
+
+        # write deckard output to file
+        $file = "/home/pi/MyNAS/deckard/".$_POST['datasetSelect'].".html";
+        file_put_contents($file, $deckard_raw);
+
+#        # get clones
+#        $clones=`./parse.sh $file`;
+#        $clones=explode(" ", $clones);
+#
+#        # add clones to database
+#        $num_clones=0;
+#        for($i=0; $i<count($clones)-1; ) {
+#            $num_frags=$clones[$i++]; 
+#            $sim=$clones[$i++];
+#
+#            $query = mysqli_query($con, 
+#                "SELECT * FROM Clones ORDER BY cloneID DESC limit 1");
+#            $cloneID=mysqli_fetch_assoc($query)['cloneID'] + 1;
+#
+#            for ($j=0; $j<$num_frags; $j++) {
+#                $datasetID=$clones[$i++];
+#                $projectID=$clones[$i++];
+#                $file=$clones[$i++];
+#                $st=$clones[$i++];
+#                $end=$clones[$i++];
+#
+#                $sql="INSERT INTO Clones (cloneID, datasetID, projectID, ".
+#                    "file, start, end, sim, detector, language) ".
+#                    "VALUES($cloneID, $datasetID, $projectID, '$file', $st, 
+#                    $end, $sim, 'deckard', '$lang')";
+#
+#                if (!mysqli_query($con, $sql)) {
+#                    die("Error: " . mysqli_error($con));
+#                }
+#                $num_clones++;
+#            }
+#        }
+#
+#        $num_classes=`grep "Number" $file | awk '{print $4}'`;
+#        echo "There were $num_classes classes of clones.<br>";
+#        echo "Added $num_clones clones pairs.";
+
+    }
+    mysqli_close($con);
 }
 
 
