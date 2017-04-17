46d0f5b5800b572740260bfe24f84d6871f414bb
Colin Lightfoot <cflightfoot@gmail.com>
2017-04-17 15:59:57 +0000

    Updated links

--- code/user/resetpass.php
+++ code/user/resetpass.php
@@ -4,7 +4,7 @@ $user = new USER();
 
 if(empty($_GET['id']) && empty($_GET['code']))
 {
- #$user->redirect('index.php');
+ #$user->redirect('../index.php');
 }
 
 if(isset($_GET['id']) && isset($_GET['code']))
@@ -50,7 +50,7 @@ if(isset($_GET['id']) && isset($_GET['code']))
        <button class='close' data-dismiss='alert'>&times;</button>
        Password Changed.
        </div>";
-     header("refresh:5;index.php");
+     header("refresh:5;../index.php");
     }
    }
   } 
