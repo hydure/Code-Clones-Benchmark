3500dd878fb7dc1b16c2da5998850e0bf5c32837
Jason Kimko <jskimko@email.wm.edu>
2017-05-01 01:04:17 +0000

    Run nicad in background.

--- code/run_detectors.php
+++ code/run_detectors.php
@@ -90,17 +90,17 @@ $(document).ready(function() {
           <h1>Code Cloning Tools</h1>
 <?php
 
-header("refresh:10;url=CCBTools.php");
+#header("refresh:10;url=CCBTools.php");
 
 if (!empty($_POST['detector'])) {
 
 
-echo "<br>You chose to run < "; 
-foreach($_POST['detector'] as $detector) {
-    echo "$detector ";
-}
-
-echo "> on dataset ".$_POST['datasetSelect']."<br>";
+#echo "<br>You chose to run < "; 
+#foreach($_POST['detector'] as $detector) {
+#    echo "$detector ";
+#}
+#
+#echo "> on dataset ".$_POST['datasetSelect']."<br>";
 
 # connect to database
 $con = mysqli_connect('localhost', 'root', '*XMmysq$', 'cc_bench');
@@ -163,20 +163,17 @@ foreach($_POST['detector'] as $detector) {
         # run nicad
         $nicad_path="/home/clone/nicad.sh";
         $out_file = "/home/pi/MyNAS/nicad/".$datasetID.".html";
-        #$cmd="ssh -o StrictHostKeyChecking=no clone@45.33.96.10 '$nicad_path $args' >$out_file 2>/dev/null &";
-        $cmd="ssh -o StrictHostKeyChecking=no clone@45.33.96.10 '$nicad_path $args' | grep -v 'known hosts' ";
-        #$cmd="$cmd >$out_file 2>/dev/null && php add_frags.php $detector $lang $userID $out_file >/dev/null 2>&1";
-        #$cmd="$cmd && bash save_frags.sh $detector $out_file $args >/dev/null 2>&1 ";
-        #$cmd="$cmd && php send_mail.php ".$_SESSION['email']." 'Job Update' 'Dataset $datasetID: job finished' ";
-        #$cmd="$cmd >/dev/null 2>&1 &";
-        #shell_exec("$cmd &");
-        #ignore_user_abort();
-        #echo "Running: sleep 10 && php add_frags.php $detector $lang $userID $out_file & &>/dev/null<br>";
-        #shell_exec("sleep 10 && nohup exec php add_frags.php $detector $lang $userID $out_file >/dev/null &");
-        #echo "Sent...<br>";
-        #echo "$cmd<br>";
-        #exec("sleep 10 >/dev/null &");
-        $nicad_raw = shell_exec($cmd);
+        $cmd="(ssh -o StrictHostKeyChecking=no clone@45.33.96.10 '$nicad_path $args' >$out_file";
+        #$cmd="ssh -o StrictHostKeyChecking=no clone@45.33.96.10 '$nicad_path $args' | grep -v 'known hosts' ";
+        $cmd="$cmd; php add_frags.php $detector $lang $userID $out_file";
+        $cmd="$cmd; bash save_frags.sh $detector $out_file $args";
+        $cmd="$cmd; php send_mail.php ".$_SESSION['email']." 'Job Finished' 'Dataset: $datasetID<br>".
+                    "Detector: NiCad'";
+        $cmd="$cmd) >/dev/null &";
+        ignore_user_abort();
+        shell_exec("$cmd");
+
+        #$nicad_raw = shell_exec($cmd);
         #echo "$nicad_raw<br>";
         
         #update tags in Datasets
@@ -187,63 +184,65 @@ foreach($_POST['detector'] as $detector) {
         if(!$con->query($sql))
             echo "failed to update dataset info";
 
-        # write nicad output to file
-        $out_file = "/home/pi/MyNAS/nicad/".$datasetID.".html";
-        file_put_contents($out_file, $nicad_raw);
+        echo "Job submitted. You will be notified by email upon completion.";
 
-        # get clones
-        $clones=`./parse.sh $out_file`;
-        $clones=explode(" ", $clones);
-
-        # add clones to database
-        $num_clones=0;
-        for($i=0; $i<count($clones)-1; ) {
-            $num_frags=$clones[$i++]; 
-            $sim=$clones[$i++];
-
-            $query = mysqli_query($con, 
-                "SELECT * FROM Clones ORDER BY cloneID DESC limit 1");
-            $cloneID=mysqli_fetch_assoc($query)['cloneID'] + 1;
-
-            for ($j=0; $j<$num_frags; $j++) {
-                $datasetID=$clones[$i++];
-                $projectID=$clones[$i++];
-                $file=$clones[$i++];
-                $st=$clones[$i++];
-                $end=$clones[$i++];
-
-                # nicad appends .pyindent, .ifdefed
-                $file=preg_replace("/\.pyindent/", "", $file);
-                $file=preg_replace("/\.ifdefed/", "", $file);
-
-                # check if clone has been added already
-                $history = mysqli_query($con, "SELECT cloneID FROM Clones WHERE ".
-                    "projectID=$projectID AND detector='nicad' AND ".
-                    "language='$lang' AND file='$file' AND start='$st' ".
-                    "AND end=$end");
-                if ($history->num_rows > 0) {
-                    continue;
-                }
-       
-                $sql="INSERT INTO Clones (cloneID, datasetID, projectID, ".
-                    "userID, file, start, end, sim, detector, language) ".
-                    "VALUES($cloneID, $datasetID, $projectID, '$userID', '$file', 
-                    $st, $end, $sim, 'nicad', '$lang')";
-
-                if (!mysqli_query($con, $sql)) {
-                    die("Error: " . mysqli_error($con));
-                }
-                $num_clones++;
-            }
-        }
-
-        $num_classes=`grep "Number" $out_file | awk '{print $4}'`;
-        echo "There were $num_classes classes of clones.<br>";
-        echo "Added $num_clones clones.";
-
-        # save files with clone fragments
-        #echo "<br>./save_frags.sh $detector $out_file $args<br>";
-        shell_exec("bash save_frags.sh $detector $out_file $args");
+#        # write nicad output to file
+#        $out_file = "/home/pi/MyNAS/nicad/".$datasetID.".html";
+#        file_put_contents($out_file, $nicad_raw);
+#
+#        # get clones
+#        $clones=`./parse.sh $out_file`;
+#        $clones=explode(" ", $clones);
+#
+#        # add clones to database
+#        $num_clones=0;
+#        for($i=0; $i<count($clones)-1; ) {
+#            $num_frags=$clones[$i++]; 
+#            $sim=$clones[$i++];
+#
+#            $query = mysqli_query($con, 
+#                "SELECT * FROM Clones ORDER BY cloneID DESC limit 1");
+#            $cloneID=mysqli_fetch_assoc($query)['cloneID'] + 1;
+#
+#            for ($j=0; $j<$num_frags; $j++) {
+#                $datasetID=$clones[$i++];
+#                $projectID=$clones[$i++];
+#                $file=$clones[$i++];
+#                $st=$clones[$i++];
+#                $end=$clones[$i++];
+#
+#                # nicad appends .pyindent, .ifdefed
+#                $file=preg_replace("/\.pyindent/", "", $file);
+#                $file=preg_replace("/\.ifdefed/", "", $file);
+#
+#                # check if clone has been added already
+#                $history = mysqli_query($con, "SELECT cloneID FROM Clones WHERE ".
+#                    "projectID=$projectID AND detector='nicad' AND ".
+#                    "language='$lang' AND file='$file' AND start='$st' ".
+#                    "AND end=$end");
+#                if ($history->num_rows > 0) {
+#                    continue;
+#                }
+#       
+#                $sql="INSERT INTO Clones (cloneID, datasetID, projectID, ".
+#                    "userID, file, start, end, sim, detector, language) ".
+#                    "VALUES($cloneID, $datasetID, $projectID, '$userID', '$file', 
+#                    $st, $end, $sim, 'nicad', '$lang')";
+#
+#                if (!mysqli_query($con, $sql)) {
+#                    die("Error: " . mysqli_error($con));
+#                }
+#                $num_clones++;
+#            }
+#        }
+#
+#        $num_classes=`grep "Number" $out_file | awk '{print $4}'`;
+#        echo "There were $num_classes classes of clones.<br>";
+#        echo "Added $num_clones clones.";
+#
+#        # save files with clone fragments
+#        #echo "<br>./save_frags.sh $detector $out_file $args<br>";
+#        shell_exec("bash save_frags.sh $detector $out_file $args");
 
         /***************************
         *                          *
@@ -285,11 +284,20 @@ foreach($_POST['detector'] as $detector) {
 
         # run deckard
         $deckard_path="/home/clone/deckard.sh";
-        #$file = "/home/pi/MyNAS/nicad/".$datasetID.".html";
-        #$cmd="ssh -o StrictHostKeyChecking=no clone@45.33.96.10 '$nicad_path $args' >$file 2>/dev/null &";
-        $cmd="ssh -o StrictHostKeyChecking=no clone@45.33.96.10 '$deckard_path $args' | grep -v 'known hosts'";
-        $deckard_raw = shell_exec($cmd);
+        $out_file = "/home/pi/MyNAS/deckard/".$datasetID."_out";
+        #$cmd="ssh -o StrictHostKeyChecking=no clone@45.33.96.10 '$deckard_path $args' | grep -v 'known hosts'";
+        #$deckard_raw = shell_exec($cmd);
         #echo "$deckard_raw<br>";
+
+        $cmd="(ssh -o StrictHostKeyChecking=no clone@45.33.96.10 '$deckard_path $args' >$out_file";
+        $cmd="$cmd; php add_frags.php $detector $lang $userID $out_file";
+        $cmd="$cmd; bash save_frags.sh $detector $out_file $args";
+        $cmd="$cmd; php send_mail.php ".$_SESSION['email']." 'Job Finished' 'Dataset: $datasetID<br>".
+                    "Detector: Deckard'";
+        $cmd="$cmd) >/dev/null &";
+        ignore_user_abort();
+        #shell_exec("$cmd");
+        echo "$cmd<br>";
 
         #update tags in Datasets
         date_default_timezone_set('America/New_York');
@@ -299,72 +307,74 @@ foreach($_POST['detector'] as $detector) {
         if(!$con->query($sql))
             echo "failed to update dataset info";
 
-        if (preg_match('/^Error: there are no/', $deckard_raw)) {
-            echo "$deckard_raw<br>";
-            break;
-        }
+        #if (preg_match('/^Error: there are no/', $deckard_raw)) {
+            #echo "$deckard_raw<br>";
+            #break;
+        #}
 
-        # write deckard output to file
-        $out_file = "/home/pi/MyNAS/deckard/".$datasetID."_out";
-        file_put_contents($out_file, $deckard_raw);
+        echo "Job submitted. You will be notified by email upon completion.";
 
-        # get cloneID
-        $query = mysqli_query($con, 
-            "SELECT * FROM Clones ORDER BY cloneID DESC limit 1");
-        $cloneID=mysqli_fetch_assoc($query)['cloneID'] + 1;
-        
-        if (!($handle = fopen($out_file, "r"))) {
-            echo "Error: cannot open file '$file'<br>";
-            break;
-        }
-
-        $num_classes=0;
-        $num_clones=0;
-        $prev="\n";
-        while (($line = fgets($handle)) != false) {
-            if ($line != "\n") {
-                $parsed = explode(" ", $line);
-                $tmp = $parsed[1];
-                $file=preg_replace(":src/[0-9]*/:", "", $tmp);
-                $projectID=preg_replace(":src/:", "", $tmp);
-                $projectID=preg_replace(":/.*:", "", $projectID);
-                $index = explode(":", substr($parsed[2], 4));
-                $start = $index[1];
-                $end = intval(($index[2])) + intval($start) - 1;
-
-                # check if clone has been added already
-                $history = mysqli_query($con, "SELECT cloneID FROM Clones WHERE ".
-                    "projectID=$projectID AND detector='deckard' AND ".
-                    "language='$lang' AND file='$file' AND start='$start' ".
-                    "AND end=$end");
-                if ($history->num_rows > 0) {
-                    continue;
-                } 
-
-                $sql = "INSERT INTO Clones (cloneID, datasetID, projectID, ".
-                        "userID, file, start, end, detector, language) ".
-                        "VALUES ( '{$cloneID}', '{$datasetID}', ".
-                        "'{$projectID}', '{$userID}','{$file}', ".
-                        "'{$start}', '{$end}', 'deckard', '$lang')";
-
-                if (!mysqli_query($con, $sql)) {
-                    die("Error: " . mysqli_error($con));
-                }
-                $num_clones++;
-            } else if ($line != $prev) { 
-                $cloneID += 1;
-                $num_classes++;
-            }
-            $prev=$line;
-        }
-        fclose($handle);
-
-        echo "There were $num_classes classes of clones.<br>";
-        echo "Added $num_clones clones.";
-
-        # save files with clone fragments
-        #echo "<br>./save_frags.sh $detector $out_file $args<br>";
-        shell_exec("./save_frags.sh $detector $out_file $args");
+#        # write deckard output to file
+#        $out_file = "/home/pi/MyNAS/deckard/".$datasetID."_out";
+#        file_put_contents($out_file, $deckard_raw);
+#
+#        # get cloneID
+#        $query = mysqli_query($con, 
+#            "SELECT * FROM Clones ORDER BY cloneID DESC limit 1");
+#        $cloneID=mysqli_fetch_assoc($query)['cloneID'] + 1;
+#        
+#        if (!($handle = fopen($out_file, "r"))) {
+#            echo "Error: cannot open file '$file'<br>";
+#            break;
+#        }
+#
+#        $num_classes=0;
+#        $num_clones=0;
+#        $prev="\n";
+#        while (($line = fgets($handle)) != false) {
+#            if ($line != "\n") {
+#                $parsed = explode(" ", $line);
+#                $tmp = $parsed[1];
+#                $file=preg_replace(":src/[0-9]*/:", "", $tmp);
+#                $projectID=preg_replace(":src/:", "", $tmp);
+#                $projectID=preg_replace(":/.*:", "", $projectID);
+#                $index = explode(":", substr($parsed[2], 4));
+#                $start = $index[1];
+#                $end = intval(($index[2])) + intval($start) - 1;
+#
+#                # check if clone has been added already
+#                $history = mysqli_query($con, "SELECT cloneID FROM Clones WHERE ".
+#                    "projectID=$projectID AND detector='deckard' AND ".
+#                    "language='$lang' AND file='$file' AND start='$start' ".
+#                    "AND end=$end");
+#                if ($history->num_rows > 0) {
+#                    continue;
+#                } 
+#
+#                $sql = "INSERT INTO Clones (cloneID, datasetID, projectID, ".
+#                        "userID, file, start, end, detector, language) ".
+#                        "VALUES ( '{$cloneID}', '{$datasetID}', ".
+#                        "'{$projectID}', '{$userID}','{$file}', ".
+#                        "'{$start}', '{$end}', 'deckard', '$lang')";
+#
+#                if (!mysqli_query($con, $sql)) {
+#                    die("Error: " . mysqli_error($con));
+#                }
+#                $num_clones++;
+#            } else if ($line != $prev) { 
+#                $cloneID += 1;
+#                $num_classes++;
+#            }
+#            $prev=$line;
+#        }
+#        fclose($handle);
+#
+#        echo "There were $num_classes classes of clones.<br>";
+#        echo "Added $num_clones clones.";
+#
+#        # save files with clone fragments
+#        #echo "<br>./save_frags.sh $detector $out_file $args<br>";
+#        shell_exec("./save_frags.sh $detector $out_file $args");
     }
 }
 mysqli_close($con);
